3cdcab6590bf6102b2960a9f8c9b92c83531cd36
Juha Lindstedt <juha@pakastin.fi>
2016-09-03 13:41:05 +0300

    added compression

--- assets/server.js
+++ assets/server.js
@@ -1,11 +1,13 @@
 var express = require('express');
+var compression = require('compression');
 
 var app = express();
 
+app.use(compression());
 app.use(express.static('public'));
 
 app.listen(8080, function (err) {
   if (err) throw err;
-  
+
   console.log('RE:DOM dev running, browse to http://localhost:8080 with your favourite browser! ;)');
 });
