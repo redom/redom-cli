751c084b10fe6b573710ffb6b2e564b1c6fe5730
Juha Lindstedt <juha@pakastin.fi>
2016-09-03 13:19:56 +0300

    updated hello world + fixed uglifyjs

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,27 @@
-import { el, mount } from 'redom';
+import { el, list, mount } from 'redom';
 
 const hello = el('h1', 'Hello world!');
 
 mount(document.body, hello);
+
+class Td {
+  constructor () {
+    this.el = el('td');
+  }
+  update (data) {
+    this.el.textContent = data;
+  }
+}
+
+const Tr = list.extend('tr', Td);
+const Table = list.extend('table', Tr);
+
+const table = new Table;
+
+mount(document.body, table);
+
+table.update([
+  [ 1, 2, 3 ],
+  [ 4, 5, 6 ],
+  [ 7, 8, 9 ]
+]);
